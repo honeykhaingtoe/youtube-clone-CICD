8b28bbf28cd28454eae150230e35882504b180f4
Hlaing Min Paing <hlaingminpaing.ygn@gmail.com>
2025-04-20 15:43:28 +0700

    update eks

--- terraform-monitoring/install.sh
+++ terraform-monitoring/install.sh
@@ -70,11 +70,11 @@ sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
 
 ##Install Grafana
-$ sudo apt-get update
-$ sudo apt-get install -y apt-transport-https software-properties-common
-$ wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-$ echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-$ sudo apt-get update
-$ sudo apt-get -y install grafana
-$ sudo systemctl enable grafana-server
-$ sudo systemctl start grafana-server
+sudo apt-get update
+sudo apt-get install -y apt-transport-https software-properties-common
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get -y install grafana
+sudo systemctl enable grafana-server
+sudo systemctl start grafana-server
