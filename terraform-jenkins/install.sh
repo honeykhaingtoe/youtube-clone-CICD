71e9b3515e5e5462a9c6aa55ff3064fe858bd764
Hlaing Min Paing <hlaingminpaing.ygn@gmail.com>
2025-04-21 20:24:22 +0700

    k8s

--- terraform-jenkins/install.sh
+++ terraform-jenkins/install.sh
@@ -46,5 +46,5 @@ eksctl version
 sudo apt update -y
 sudo apt install curl -y
 curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl -y
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl 
 kubectl version --client
